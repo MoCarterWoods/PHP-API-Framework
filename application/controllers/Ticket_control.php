75ef7a11105f969acf7a5b7675b55bf856949f9d
MoCarterWoods <kantapong_mo@hotmail.com>
2024-02-12 17:02:43 +0700

    12/2/2024

--- application/controllers/Ticket_control.php
+++ application/controllers/Ticket_control.php
@@ -22,4 +22,29 @@ class Ticket_control extends CI_Controller {
         echo json_encode($result);
     } 
 
+    public function accept_ticket(){
+        $data = unserialize($this->input->post('data'));
+        $sess = unserialize($this->input->post('session'));
+        $result = $this->tkc->accept_ticket($data,$sess);
+       
+        echo json_encode($result);
+    }
+
+    public function cancel_ticket(){
+        $data = unserialize($this->input->post('data'));
+        $sess = unserialize($this->input->post('session'));
+        $result = $this->tkc->cancel_ticket($data,$sess);
+       
+        echo json_encode($result);
+    }
+
+    public function show_equipment(){
+        $data = $this->input->post();
+       
+        // $data = unserialize($this->input->post('data'));
+        $result = $this->tkc->show_equipment($data);
+        // echo "<pre>";
+        // print_r($result);
+        echo json_encode($result);
+    } 
 }
--- application/models/Ticket_control_model.php
+++ application/models/Ticket_control_model.php
@@ -13,53 +13,67 @@ class Ticket_control_model extends CI_Model
 
     public function show_data()
     {
-        $sql = "SELECT 
-        t1.ist_id, 
-        t1.ist_type, 
-        t1.ist_pd, 
-        t1.ist_line_cd, 
-        t1.ist_area_other, 
-        t1.ist_process, 
-        t1.ist_tool, 
-        t1.ist_job_no,
-        t1.mjt_id, 
+        $sql = "SELECT
+        t1.ist_id,
+        t1.ist_type,
+        t1.ist_pd,
+        t1.ist_line_cd,
+        t1.ist_area_other,
+        t1.ist_process,
         t1.ist_request_by,
-        t1.ist_status_flg, 
         t2.mjt_name_eng,
         t2.mjt_name_thai,
-        GROUP_CONCAT(DISTINCT t3.lmw_id) AS lmw_id,
-        GROUP_CONCAT(DISTINCT t4.swa_fristname) AS swa_fristname,
-        GROUP_CONCAT(DISTINCT t4.swa_lastname) AS swa_lastname,
-        GROUP_CONCAT(DISTINCT t4.swa_emp_code) AS swa_emp_code,
-        t5.mts_name
-    FROM 
+        GROUP_CONCAT( DISTINCT t3.lmw_id ) AS lmw_id,
+        GROUP_CONCAT( DISTINCT t4.swa_fristname ) AS swa_fristname,
+        GROUP_CONCAT( DISTINCT t4.swa_emp_code ) AS swa_emp_code,
+        t5.mts_name,
+        CASE
+         WHEN t1.ist_line_cd OR t1.ist_area_other IS NULL THEN
+         1 ELSE 3
+        END AS equipment_status,
+        t6.ipc_status_flg AS ploblem_status,
+        CASE
+         WHEN t1.mjt_id  IS NULL THEN
+         1 ELSE 3
+        END AS jopType_status,
+        t7.iim_status_flg AS inspection_status,
+        t8.it_status_flg AS troubleshooting_status,
+        t9.irp_status_flg AS rqPart_status,
+        t10.iap_status_flg AS analyze_status,
+        t11.ipr_status_flg AS prevention_status,
+        t12.ide_status_flg AS delivery_status,
+        t1.ist_status_flg
+       FROM
         info_issue_ticket t1
-    LEFT JOIN 
-        mst_job_type t2 ON t1.mjt_id = t2.mjt_id
-    LEFT JOIN 
-        log_manage_worker t3 ON t1.ist_id = t3.ist_id
-    LEFT JOIN 
-        sys_worker_app t4 ON t3.lmw_worker = t4.swa_id
-    LEFT JOIN
-        mst_tooling_system t5 ON t1.ist_tool = t5.mts_id
-    WHERE 
-        t1.ist_status_flg IN (1, 3, 5, 7, 8)
-    GROUP BY 
-        t1.ist_id, 
-        t1.ist_type, 
-        t1.ist_pd, 
-        t1.ist_line_cd, 
-        t1.ist_area_other, 
-        t1.ist_process, 
-        t1.ist_tool, 
+        LEFT JOIN mst_job_type t2 ON t1.mjt_id = t2.mjt_id
+        LEFT JOIN log_manage_worker t3 ON t1.ist_id = t3.ist_id
+        LEFT JOIN sys_worker_app t4 ON t3.lmw_worker = t4.swa_id
+        LEFT JOIN mst_tooling_system t5 ON t1.ist_tool = t5.mts_id
+        LEFT JOIN info_problem_condition t6 ON t1.ist_id = t6.ist_id
+        LEFT JOIN info_inspection_method t7 ON t1.ist_id = t7.ist_id
+        LEFT JOIN info_troubleshooting t8 ON t1.ist_id = t8.ist_id
+        LEFT JOIN info_required_parts t9 ON t1.ist_id = t9.ist_id
+        LEFT JOIN info_analyze_problem t10 ON t1.ist_id = t10.ist_id
+        LEFT JOIN info_prevention_recurrence t11 ON t1.ist_id = t11.ist_id
+        LEFT JOIN info_delivery_equipment t12 ON t1.ist_id = t12.ist_id 
+       WHERE
+        t1.ist_status_flg IN ( 1, 3, 5, 7, 8 ) 
+       GROUP BY
+        t1.ist_id,
+        t1.ist_type,
+        t1.ist_pd,
+        t1.ist_line_cd,
+        t1.ist_area_other,
+        t1.ist_process,
+        t1.ist_tool,
         t1.ist_job_no,
-        t1.mjt_id, 
+        t1.mjt_id,
         t1.ist_request_by,
-        t1.ist_status_flg, 
+        t1.ist_status_flg,
         t2.mjt_name_eng,
         t2.mjt_name_thai,
-        t5.mts_name
-    ORDER BY 
+        t5.mts_name 
+       ORDER BY
         t1.ist_job_no;";
 
         $query = $this->db->query($sql);
@@ -94,4 +108,57 @@ class Ticket_control_model extends CI_Model
         $data = $query->result();
         return $data;
     }
+
+    public function accept_ticket($data, $sess)
+    {
+        $stAccept = $data["newStatus"];
+        $acId = $data["acId"];
+    
+        $sql = "UPDATE info_issue_ticket 
+        SET ist_status_flg = '$stAccept', ist_accept_by = '$sess', ist_updated_date = NOW(), ist_updated_by = '$sess'
+        WHERE ist_id = '$acId';";
+    
+        $query = $this->db->query($sql);
+        return $this->db->affected_rows() > 0;
+    }
+    
+
+    public function cancel_ticket($data, $sess)
+    {
+        $DtCancle = $data["DetailCancle"];
+        $stCancle = $data["newStatus"];
+        $ccID = $data["ccID"];
+    
+        $sql = "UPDATE info_issue_ticket 
+                SET ist_status_flg = '$stCancle', ist_cancle_detail = '$DtCancle', ist_updated_date = NOW(), ist_updated_by = '$sess'
+                WHERE ist_id = '$ccID';";
+    
+        $query = $this->db->query($sql);
+        return $this->db->affected_rows() > 0 ? 1 : 2;
+    } 
+    
+    public function show_equipment($data)
+    {
+        $id = $data["ist_Id"];
+        $sql_show_eq = "SELECT 
+        ist_pd,
+        ist_line_cd,
+        ist_area_other,
+        ist_process,
+        ist_tool
+         
+        FROM info_issue_ticket 
+        WHERE ist_id = '$id';";
+
+        $query = $this->db->query($sql_show_eq);
+        $data = $query->row();
+
+        if ($this->db->affected_rows() > 0) {
+            return array('result' => true, 'data' => $data);
+        } else {
+            return array('result' => false);
+        }
+    }
+
 }
+
